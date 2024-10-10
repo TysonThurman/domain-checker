2b69713e37dbd2ad7a33366a2b4c30026fd42d6c
Tyson Thurman <tthurman@Tysons-Laptop.local>
2024-10-09 21:07:43 -0700

    added checkbox for collapse

--- client/src/FormComponent.jsx
+++ client/src/FormComponent.jsx
@@ -7,6 +7,7 @@ const API_URL = 'http://localhost:5001';
 const FormComponent = () => {
   const [inputValue, setInputValue] = useState('');
   const [result, setResult] = useState(0);
+  const [collapsed, setCollapsed] = useState(false);
 
   const handleInputChange = (e) => {
     setInputValue(e.target.value);
@@ -28,6 +29,10 @@ const FormComponent = () => {
     setInputValue('');
   };
 
+  const handleCollapse = () => {
+    setCollapsed(!collapsed);
+  };
+
   return (
     <div>
       <form onSubmit={handleSubmit}>
@@ -38,9 +43,12 @@ const FormComponent = () => {
           placeholder="Enter something..."
         />
         <button type="submit">Submit</button>
+        <br /><br />
+        <label htmlFor="collapsed">Collapse All</label>
+        <input onChange={handleCollapse} id="collapsed" type="checkbox" />
       </form>
       <div id="resultdiv">
-      {result ? <ReactJson src={result} displayDataTypes={false} theme={"monokai"} name={result.domainName} collapsed={true} /> : ''}
+      {result ? <ReactJson src={result} displayDataTypes={false} theme={"monokai"} name={result.domainName} collapsed={collapsed} /> : ''}
       </div>
 
     </div>
